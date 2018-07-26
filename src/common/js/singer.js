8dbe39fc0af065460400e4a69dec2e8b2023757c
毅杨 <yangyi@gogopay.cn>
2018-07-26 17:41:24 +0800

    Initial commit

---
+++ src/common/js/singer.js
@@ -0,0 +1,8 @@
+export default class Singer {
+  constructor({id, name}) {
+    this.id = id
+    this.name = name
+    this.avatar = `https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?`
+  }
+}
+// y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg
