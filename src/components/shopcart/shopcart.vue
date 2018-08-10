831c4b46f98f9d95d69d80b580fcc2fab0fa333f
毅杨 <yangyi@gogopay.cn>
2018-08-10 17:39:22 +0800

    [Feature]提交代码

---
+++ src/components/shopcart/shopcart.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="shopcart">
+    <div class="content">
+      <div class="content-left">
+        <div class="logo-wrapper">
+          <div class="logo">
+            <i class="icon-shopping_cart"></i>
+          </div>
+        </div>
+        <div class="price">0元</div>
+        <div class="desc">fdsfdsfdsfds</div>
+      </div>
+      <div class="content-right">
+        <div class="pay">多少多少元</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .shopcart
+    position fixed
+    left 0
+    bottom 0
+    z-index 50
+    width 100%
+    height 48px
+    .content
+      display flex
+      background #141d27
+      height 48px
+      font-size 0
+      .content-left
+        flex 1
+        .logo-wrapper
+          display: inline-block
+          vertical-align: top
+          position: relative
+          top: -10px
+          margin: 0 12px
+          padding: 6px
+          width: 56px
+          height: 56px
+          box-sizing: border-box
+          border-radius: 50%
+          background: #141d27
+          .logo
+            width 100%
+            height 100%
+            border-radius: 50%
+            background #2b343c
+            text-align center
+            .icon-shopping_cart
+              font-size 24px
+              color #80858a
+              line-height 44px
+        .price
+          display inline-block
+          vertical-align: top
+          line-height 24px
+          margin-top 12px
+          box-sizing border-box
+          padding-right 12px
+          border-right 1px solid rgba(255, 255, 255, 0.1)
+          font-size 16px
+          font-weight 700
+          color rgba(255, 255, 255, 0.4)
+        .desc
+          display inline-block
+          vertical-align: top
+          line-height 24px
+          margin-left 12px
+          margin-top 12px
+          font-size 16px
+          color rgba(255, 255, 255, 0.4)
+      .content-right
+        width 105px
+        flex: 0 0 105px
+        .pay
+          height: 48px
+          line-height: 48px
+          text-align: center
+          font-size: 12px
+          font-weight: 700
+          background: #2b333b
+</style>
